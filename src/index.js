9a2d641ae4c9d8b96551197c057f38dea917325e
ArkadiiRaih <ArkadioRaih@yandex.ru>
2019-11-18 14:36:26 +1000

    Added css-loader and style-loader to prod and dev

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import nav from "./nav";
 import { footer } from "./footer";
 import makeButton from "./button";
 import { makeColorStyle } from "./button-styles";
+import footerCss from"./footer.css";
+import buttonCss from "./button.css";
 
 const button = makeButton("Yay! A Button!");
 button.style = makeColorStyle("cyan");
